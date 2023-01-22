c5f0c54b390841ac7fd2bb502c873477c5fe58cd
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2023-01-23 01:03:49 +0545

    feat(layout): implemented main layout :rocket:

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        corporate: {
+          ...require('daisyui/src/colors/themes')['[data-theme=corporate]'],
+          'base-200': '#fff',
+          'base-content': '#444E59',
+        },
+      },
+      'night',
+    ],
+  },
+};
