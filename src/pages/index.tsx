c87169464faeef2e307d1d0f2f4ddcff8398d222
Nimesh Maharjan <maharjannimesh11@gmail.com>
2023-08-07 21:15:37 +0545

    feat(home): home page revamp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,130 @@
-import ProductCard from '@/features/products/components/product-card';
-import BrandsScrolling from '@/shared/components/brands-scrolling';
-import { IProduct } from '@/shared/interfaces/product.interface';
-import MainSharedLayout from '@/shared/layouts/main';
-import axios from 'axios';
-import { motion } from 'framer-motion';
-import { ArrowDown } from 'lucide-react';
-import { useQuery } from 'react-query';
-import { NextPageWithLayout } from './_app';
+import ProductCard from "@/features/products/components/product-card";
+import BrandsScrolling from "@/shared/components/brands-scrolling";
+import { IProduct } from "@/shared/interfaces/product.interface";
+import MainSharedLayout from "@/shared/layouts/main";
+import axios from "axios";
+import { motion } from "framer-motion";
+import {
+  ArrowDown,
+  BadgeCheck,
+  Boxes,
+  CalendarHeart,
+  Sparkles,
+} from "lucide-react";
+import { useQuery } from "react-query";
+import { NextPageWithLayout } from "./_app";
+
+// const Home: NextPageWithLayout = () => {
+//   const { data: featuredProducts, isLoading: isTopSellingProductLoading } =
+//     useQuery<IProduct[]>("fetchFeaturedProducts", async () => {
+//       const res = await axios.get("/api/products?page=1&limit=4");
+//       return res.data.products;
+//     });
+//   return (
+//     <>
+//       <section className="flex items-center justify-center relative mx-auto w-full min-h-[calc(100vh-10rem)] max-w-[64rem] flex-col  gap-4 text-center">
+//         <motion.h2
+//           initial={{ opacity: 0, y: -20 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           transition={{ duration: 0.8, delay: 0.5 }}
+//           className="text-3xl font-bold leading-tight tracking-normal md:text-5xl lg:text-6xl lg:leading-[1.1]"
+//         >
+//           Discover the Latest Appliances at{" "}
+//           <span className="text-primary">
+//             Eeshan Mahadev Enterprises Private Limited.
+//           </span>
+//         </motion.h2>
+//         <motion.p
+//           initial={{ opacity: 0, y: -10 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           transition={{ duration: 0.8, delay: 1 }}
+//           className="max-w-[46rem] text-lg text-muted-foreground sm:text-xl"
+//         >
+//           <span className="max-w-[46rem] text-lg text-opacity-60 sm:text-xl">
+//             Explore a wide range of premium denim products from top brands and
+//             independent stores worldwide.
+//           </span>
+//         </motion.p>
+//         <motion.button
+//           initial={{ opacity: 0, y: -10 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           transition={{ duration: 0.8, delay: 1.5 }}
+//           className="btn btn-primary mt-2"
+//         >
+//           Shop Now
+//         </motion.button>
+//         <motion.div
+//           initial={{ opacity: 0, y: -30 }}
+//           animate={{ opacity: 1, y: 0 }}
+//           transition={{ duration: 0.8, delay: 2 }}
+//           className="bg-primary h-8 w-8 rounded-full absolute bottom-0 lg:bottom-0 right-0 animate-bounce flex items-center justify-center"
+//         >
+//           <ArrowDown className="text-neutral-content"></ArrowDown>
+//         </motion.div>
+//       </section>
+//       <section className="mt-24 lg:mt-32">
+//         <BrandsScrolling></BrandsScrolling>
+//       </section>
+//       <section className="mt-24 lg:mt-32">
+//         <h2 className="brands font-bold text-2xl md:text-3xl mb-8">
+//           Featured Products
+//         </h2>
+//         <div className="grid grid-cols-4">
+//           {featuredProducts?.map((product) => (
+//             <div
+//               className="col-span-6 md:col-span-2 lg:col-span-1"
+//               key={product.id}
+//             >
+//               <ProductCard product={product}></ProductCard>
+//             </div>
+//           ))}
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Home;
+
+import React from "react";
+import Image from "next/image";
+import WhyChooseUs from "@/features/home/components/why-choose-us";
+import Link from "next/link";
 
 const Home: NextPageWithLayout = () => {
-  const { data: featuredProducts, isLoading: isTopSellingProductLoading } = useQuery<IProduct[]>('fetchFeaturedProducts', async () => {
-    const res = await axios.get('/api/products?page=1');
-    return res.data.products;
-  });
   return (
     <>
-      <section className="flex items-center justify-center relative mx-auto w-full min-h-[calc(100vh-10rem)] max-w-[64rem] flex-col  gap-4 text-center">
-        <motion.h2
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.5 }}
-          className="text-3xl font-bold leading-tight tracking-normal md:text-5xl lg:text-6xl lg:leading-[1.1]">
-          Discover the Latest Appliances at <span className="text-primary">Eeshan Mahadev Enterprises Private Limited.</span>
-        </motion.h2>
-        <motion.p
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 1 }}
-          className="max-w-[46rem] text-lg text-muted-foreground sm:text-xl">
-          <span className="max-w-[46rem] text-lg text-opacity-60 sm:text-xl">
-            Explore a wide range of premium denim products from top brands and independent stores worldwide.
-          </span>
-        </motion.p>
-        <motion.button
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 1.5 }}
-          className="btn btn-primary mt-2">
-          Shop Now
-        </motion.button>
-        <motion.div
-          initial={{ opacity: 0, y: -30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 2 }}
-          className="bg-primary h-8 w-8 rounded-full absolute bottom-0 lg:bottom-0 right-0 animate-bounce flex items-center justify-center">
-          <ArrowDown className="text-neutral-content"></ArrowDown>
-        </motion.div>
-      </section>
-      <section className="mt-24 lg:mt-32">
-        <BrandsScrolling></BrandsScrolling>
-      </section>
-      <section className="mt-24 lg:mt-32">
-        <h2 className="brands font-bold text-2xl md:text-3xl mb-8">Featured Products</h2>
-        <div className="grid grid-cols-6 gap-6 md:gap-12">
-          {featuredProducts?.map((product) => (
-            <div className="col-span-6 md:col-span-2 lg:col-span-1" key={product.id}>
-              <ProductCard product={product}></ProductCard>
-            </div>
-          ))}
+      <Image
+        className="h-96 object-cover w-full"
+        height={800}
+        width={1920}
+        src={"/home/landing.jpg"}
+        quality={100}
+        alt="Shop Image"
+      ></Image>
+      <section
+        className="container grid grid-cols-6"
+        style={{ padding: "80px 64px" }}
+      >
+        <div className="col-span-3">
+          <h2 className="text-4xl font-bold leading-normal">
+            Welcome to <br /> Eeshan Mahadev Enterprises
+          </h2>
+        </div>
+        <div className="col-span-3">
+          <p className="prose">
+            At Eeshan Mahadev Enterprises Pvt. Ltd., we take pride in being one
+            of the leading distribution companies in Kathmandu, Nepal. With over
+            a decade of experience, we have established ourselves as a reliable
+            partner for more than 10 prominent companies in the home appliances
+            and kitchenware industry.
+          </p>
+          <Link href="/products" passHref>
+            <button className="btn btn-primary mt-6">Shop Now</button>
+          </Link>
         </div>
       </section>
+      <WhyChooseUs></WhyChooseUs>
     </>
   );
 };
@@ -69,9 +134,11 @@ export default Home;
 Home.getLayout = (page) => (
   <MainSharedLayout
     metaData={{
-      title: 'Home',
-      description: 'Home page for Eeshan Mahadev Enterprises Private Limited which is located at Bangemudha, Asson, Kathmandu, Nepal',
-    }}>
+      title: "Home",
+      description:
+        "Home page for Eeshan Mahadev Enterprises Private Limited which is located at Bangemudha, Asson, Kathmandu, Nepal",
+    }}
+  >
     {page}
   </MainSharedLayout>
 );
--- src/shared/layouts/main/index.tsx
+++ src/shared/layouts/main/index.tsx
@@ -1,66 +1,59 @@
-import { Inter, Poppins, Work_Sans } from '@next/font/google';
-import Head from 'next/head';
-import React, { ReactNode, useEffect, useRef, useState } from 'react';
-import MainSharedFooter from './footer';
+import { Inter } from "@next/font/google";
+import Head from "next/head";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
+import MainSharedFooter from "./footer";
 
-import Cart from '@/shared/components/cart';
-import ThemeToggler from '@/shared/components/theme-toggler';
-import { useCartStore } from '@/store/user-cart';
-import { USER_ROLES } from '@prisma/client';
-import { BadgeInfo, Bug, LayoutGrid, User } from 'lucide-react';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { AiOutlineLogout, AiOutlineUser } from 'react-icons/ai';
-import { FaBox } from 'react-icons/fa';
-import { FiUserPlus } from 'react-icons/fi';
-import { GiHamburgerMenu, GiSettingsKnobs } from 'react-icons/gi';
-import { MdLogin, MdLogout } from 'react-icons/md';
-import { RxDashboard } from 'react-icons/rx';
-import { useRouter } from 'next/router';
-import UserProfileModal from '@/features/user/profile-modal';
-import NavAvatarDropdown from '@/features/user/avatar-dropdown';
-import { Toast, showToast } from '@/shared/utils/toast.util';
-import axios from 'axios';
-import Combobox from '@/shared/components/combobox';
-import { useInfiniteQuery } from 'react-query';
-import { getAllProducts } from '@/features/admin/services/products/products.service';
+import NavAvatarDropdown from "@/features/user/avatar-dropdown";
+import UserProfileModal from "@/features/user/profile-modal";
+import Cart from "@/shared/components/cart";
+import ThemeToggler from "@/shared/components/theme-toggler";
+import { Toast, showToast } from "@/shared/utils/toast.util";
+import { useCartStore } from "@/store/user-cart";
+import { USER_ROLES } from "@prisma/client";
+import axios from "axios";
+import { BadgeInfo, Bug, LayoutGrid } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { FaBox } from "react-icons/fa";
+import { FiUserPlus } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdLogin } from "react-icons/md";
 
-import { useInView } from 'react-intersection-observer';
-import Drawer from '@/shared/components/drawer';
-import { LogOut } from 'lucide-react';
+import classNames from "classnames";
+import { LogOut } from "lucide-react";
+import { useInView } from "react-intersection-observer";
 
 const inter = Inter({
   preload: false,
-  fallback: ['system-ui'],
-  subsets: ['latin'],
-  weight: ['200', '300', '400', '500', '600', '700', '800'],
+  fallback: ["system-ui"],
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
-// const inter = Work_Sans({
-//   preload: false,
-//   subsets: ['latin'],
-//   fallback: ['system-ui'],
-//   weight: ['200', '300', '400', '500', '600', '700'],
-// });
-
-const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: string; description?: string } }> = ({
-  children,
-  metaData: { title, description },
-}) => {
+const MainSharedLayout: React.FC<{
+  children: ReactNode;
+  metaData: { title?: string; description?: string; isHome?: boolean };
+}> = ({ children, metaData: { title, description, isHome } }) => {
   const { data: session, status } = useSession();
-  const [isAdmin, setIsAdmin] = useState(session?.user?.role === USER_ROLES.SUPER_ADMIN || session?.user?.role === USER_ROLES.STAFF);
+  const [isAdmin, setIsAdmin] = useState(
+    session?.user?.role === USER_ROLES.SUPER_ADMIN ||
+      session?.user?.role === USER_ROLES.STAFF
+  );
   const [isMounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
   useEffect(() => {
-    setIsAdmin(session?.user?.role === USER_ROLES.SUPER_ADMIN || session?.user?.role === USER_ROLES.STAFF);
+    setIsAdmin(
+      session?.user?.role === USER_ROLES.SUPER_ADMIN ||
+        session?.user?.role === USER_ROLES.STAFF
+    );
   }, [session?.user?.role]);
 
   const { setCartItems } = useCartStore();
 
   useEffect(() => {
     if (window.localStorage) {
-      const cartItems = localStorage.getItem('cartItems');
+      const cartItems = localStorage.getItem("cartItems");
       if (cartItems) {
         setCartItems(JSON.parse(cartItems));
       }
@@ -82,7 +75,7 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
         return;
       } else {
         setTimeout(() => {
-          showToast(Toast.warning, 'Please add your phone number.');
+          showToast(Toast.warning, "Please add your phone number.");
           setIsFromNoPhoneNumber(true);
           profileModalRef.current?.show();
         }, 10000);
@@ -95,7 +88,7 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
 
   const { ref, inView } = useInView();
   const limit = 10;
-  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   // const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
   //   'getAllProducts',
@@ -119,31 +112,48 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
   if (!isMounted) return null;
 
   return (
-    <div className={`min-h-screen flex flex-col justify-between scroll-smooth antialiased ${inter.className}`}>
+    <div
+      className={`min-h-screen flex flex-col justify-between scroll-smooth antialiased ${inter.className}`}
+    >
       <Head>
-        <title>{title ? `EME - ${title}` : 'EME'}</title>
+        <title>{title ? `EME - ${title}` : "EME"}</title>
         <meta
           name="description"
-          content={description ? description : 'Check out new products listed from various vendors all around Nepal.'}
+          content={
+            description
+              ? description
+              : "Check out new products listed from various vendors all around Nepal."
+          }
         />
       </Head>
       {session?.user?.id && (
         <UserProfileModal
           setIsFromNoPhoneNumber={setIsFromNoPhoneNumber}
           isFromNoPhoneNumber={isFromNoPhoneNumber}
-          ref={profileModalRef}></UserProfileModal>
+          ref={profileModalRef}
+        ></UserProfileModal>
       )}
       <div className="drawer">
-        <input aria-label="toggle drawer" id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <input
+          aria-label="toggle drawer"
+          id="my-drawer-3"
+          type="checkbox"
+          className="drawer-toggle"
+        />
         <div className="flex flex-col drawer-content">
           <div className="shadow nav-wrapper bg-base-100">
-            <div className="justify-between w-full gap-3 h-26 navbar lg:container lg:mx-auto md:px-8 xl:px-28">
+            <div className="justify-between w-full gap-3 h-26 navbar lg:container lg:mx-auto md:px-8 xl:px-0">
               <div className="flex-none lg:hidden">
-                <label htmlFor="my-drawer-3" className="btn btn-sm btn-square btn-ghost">
+                <label
+                  htmlFor="my-drawer-3"
+                  className="btn btn-sm btn-square btn-ghost"
+                >
                   <GiHamburgerMenu></GiHamburgerMenu>
                 </label>
               </div>
-              <h2 className="text-xl font-semibold text-primary inline lg:hidden">EME</h2>
+              <h2 className="text-xl font-semibold text-primary inline lg:hidden">
+                EME
+              </h2>
               <div className="items-center gap-x-6 hidden lg:flex">
                 {/* <Link href="/products" className="relative w-[5.5rem] mb-1 h-14 md:w-[7.5rem] md:h-20"> */}
                 {/*   <Image src="/logo.png" fill alt="logo"></Image> */}
@@ -159,8 +169,10 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
                 </Link>
               </div>
               <div className="flex items-center gap-2 lg:hidden theme">
-                {router.pathname !== '/checkout' && <Cart></Cart>}
-                {status === 'authenticated' && <NavAvatarDropdown {...{ profileModalRef }} />}
+                {router.pathname !== "/checkout" && <Cart></Cart>}
+                {status === "authenticated" && (
+                  <NavAvatarDropdown {...{ profileModalRef }} />
+                )}
                 <ThemeToggler></ThemeToggler>
               </div>
               <div className="items-center flex-none hidden gap-4 lg:flex">
@@ -169,43 +181,59 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
                     Dashboard
                   </Link>
                 )} */}
-                {router.pathname !== '/raise-issue' && (
-                  <div className="mr-3">
+                {router.pathname !== "/raise-issue" && (
+                  <div className="mr-0">
                     <button
-                      className="btn btn-secondary btn-sm"
+                      className="btn btn-ghost btn-sm"
                       onClick={() => {
                         if (!session) {
-                          showToast(Toast.warning, 'Please login first.');
+                          showToast(Toast.warning, "Please login first.");
                           return signIn();
                         } else {
-                          router.push('/raise-issue');
+                          router.push("/raise-issue");
                         }
-                      }}>
+                      }}
+                    >
                       Issue Assistance
                     </button>
                   </div>
                 )}
-                {router.pathname !== '/checkout' && <Cart></Cart>}
+                {router.pathname !== "/checkout" && <Cart></Cart>}
                 {/* <button className="btn btn-sm btn-ghost ">Contact</button> */}
 
-                {status === 'unauthenticated' && (
+                {status === "unauthenticated" && (
                   <>
-                    <Link className="btn btn-sm btn-ghost " href="/api/auth/signin">
+                    <Link
+                      className="btn btn-sm btn-ghost "
+                      href="/api/auth/signin"
+                    >
                       Sign In
                     </Link>
-                    <Link href="/auth/register" className="btn btn-sm btn-primary ">
+                    <Link
+                      href="/auth/register"
+                      className="btn btn-sm btn-primary "
+                    >
                       Sign Up
                     </Link>
                   </>
                 )}
                 <ThemeToggler></ThemeToggler>
-                {status === 'authenticated' && <NavAvatarDropdown {...{ profileModalRef }} />}
+                {status === "authenticated" && (
+                  <NavAvatarDropdown {...{ profileModalRef }} />
+                )}
               </div>
             </div>
           </div>
 
           <main className="flex-1">
-            <div className="lg:container lg:mx-auto px-6 xl:px-28 my-6 md:my-10 md:mb-[6.6rem] min-h-[calc(100vh-440px)] ">{children}</div>
+            <div
+              className={classNames("", {
+                "lg:container lg:mx-auto px-6 xl:px-0 my-6 md:my-10 md:mb-[6.6rem] min-h-[calc(100vh-440px)]":
+                  !isHome,
+              })}
+            >
+              {children}
+            </div>
             <MainSharedFooter></MainSharedFooter>
           </main>
         </div>
@@ -213,17 +241,21 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
           <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
           <ul className="w-64 h-full p-2 bg-base-100">
             <li>
-              <Link href="/products" className="flex items-center gap-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary">
+              <Link
+                href="/products"
+                className="flex items-center gap-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary"
+              >
                 <FaBox></FaBox>
                 Products
               </Link>
             </li>
-            {status === 'unauthenticated' && (
+            {status === "unauthenticated" && (
               <>
                 <li className="-ml-[0.3rem]">
                   <Link
                     href="/api/auth/signin"
-                    className="flex items-center gap-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary">
+                    className="flex items-center gap-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary"
+                  >
                     <MdLogin className="text-lg" />
                     Sign In
                   </Link>
@@ -231,7 +263,8 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
                 <li>
                   <Link
                     href="/auth/register"
-                    className="flex items-center gap-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary">
+                    className="flex items-center gap-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary"
+                  >
                     <FiUserPlus />
                     Sign Up
                   </Link>
@@ -242,7 +275,8 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
               <li>
                 <Link
                   href="/admin/dashboard"
-                  className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary">
+                  className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary"
+                >
                   <LayoutGrid />
                   Dashboard
                 </Link>
@@ -251,25 +285,28 @@ const MainSharedLayout: React.FC<{ children: ReactNode; metaData: { title?: stri
             <li>
               <Link
                 href="/raise-issue"
-                className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary">
+                className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary"
+              >
                 <Bug />
                 Issue Assistance
               </Link>
             </li>
             <li>
-              {' '}
+              {" "}
               <Link
                 href="/about"
-                className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary">
+                className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary"
+              >
                 <BadgeInfo />
                 About
               </Link>
             </li>
-            {status === 'authenticated' && (
+            {status === "authenticated" && (
               <li>
                 <Link
                   href="/admin/dashboard"
-                  className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary">
+                  className="flex items-center gap-2 pl-2 p-3 transition-all rounded-lg hover:bg-base-200 hover:text-primary"
+                >
                   <LogOut />
                   Logout
                 </Link>
