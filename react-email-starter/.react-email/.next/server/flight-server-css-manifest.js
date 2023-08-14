a39893c47cf65f0ed695cfdcfa9682f4db9049bf
Nimesh Maharjan <maharjannimesh11@gmail.com>
2023-08-14 23:30:39 +0545

    feat: implmenting sending emails

---
+++ react-email-starter/.react-email/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/home/nimesh/Projects/ems/react-email-starter/.react-email/src/app/preview/[slug]/page": [
+      "/home/nimesh/Projects/ems/react-email-starter/.react-email/src/styles/globals.css",
+      "/home/nimesh/Projects/ems/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "/home/nimesh/Projects/ems/react-email-starter/.react-email/src/app/layout.tsx": [
+    "/home/nimesh/Projects/ems/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "/home/nimesh/Projects/ems/react-email-starter/.react-email/src/styles/globals.css"
+  ],
+  "/home/nimesh/Projects/ems/react-email-starter/.react-email/src/app/preview/[slug]/page.tsx": [
+    "/home/nimesh/Projects/ems/react-email-starter/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "/home/nimesh/Projects/ems/react-email-starter/.react-email/src/styles/globals.css"
+  ]
+}
---
+++ react-email-starter/.react-email/emails/spring-sales.tsx
@@ -0,0 +1,2 @@
+import Mail from '../../emails/spring-sales.tsx';
+export default Mail;
