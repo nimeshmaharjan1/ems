adb1f57bca3f16ff1d1b9b842152aaf39bcd3cc4
Nimesh Maharjan <maharjannimesh11@gmail.com>
2024-02-11 15:46:14 +0545

    fix: price lte gte & orders and complaints casacde delete

--- src/pages/api/email/index.tsx
+++ src/pages/api/email/index.tsx
@@ -1,11 +1,14 @@
-import { OrderEmail } from '@/features/email/order';
-import { prisma } from '@/shared/utils/db';
-import { transporter } from '@/shared/utils/email-transporter.util';
-import { render } from '@react-email/render';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { OrderEmail } from "@/features/email/order";
+import { prisma } from "@/shared/utils/db";
+import { transporter } from "@/shared/utils/email-transporter.util";
+import { render } from "@react-email/render";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     try {
       const { name, email, orderId } = req.body;
       const order = await prisma.order.findUnique({
@@ -24,38 +27,41 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
       if (!order) {
         return res.status(404).json({
-          message: 'Order not found.',
+          message: "Order not found.",
         });
       }
 
       const emailHtml = render(OrderEmail(order));
 
       const mailOptions = {
-        from: 'Eeshan Mahadev 👥 <eeshanmahadev.enterprises@gmail.com>',
+        from: "Eeshan Mahadev 👥 <eeshanmahadev.enterprises@gmail.com>",
         to: order.user.email as string,
-        subject: 'Eeshan Mahadev Enterprises💐 | Order',
+        subject: "Eeshan Mahadev Enterprises💐 | Order",
         html: emailHtml,
       };
-      console.log(mailOptions);
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
           console.error(error);
-          return res.status(400).json({ message: 'Error: Could not send email' });
+          return res
+            .status(400)
+            .json({ message: "Error: Could not send email" });
         }
 
-        console.log('Email sent: ' + info.response);
-        return res.status(200).json({ message: 'Email has been sent.' });
+        console.log("Email sent: " + info.response);
+        return res.status(200).json({ message: "Email has been sent." });
       });
     } catch (error) {
-      console.error('Error sending email:', error);
+      console.error("Error sending email:", error);
       res.status(500).json({
-        message: 'Failed to send the email please try again later again.',
+        message: "Failed to send the email please try again later again.",
       });
     } finally {
       await prisma.$disconnect();
     }
   } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).json({ message: `HTTP method ${req.method} is not supported.` });
+    res.setHeader("Allow", ["POST"]);
+    res
+      .status(405)
+      .json({ message: `HTTP method ${req.method} is not supported.` });
   }
 }
